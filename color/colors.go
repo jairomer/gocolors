9ec1c96484c1b31d93f2e6d962cb418b68c3c466
neusynk <jairomer@protonmail.com>
2021-06-30 18:56:46 +0200

    Removing Windows support.

--- color/colors.go
+++ color/colors.go
@@ -1,10 +1,6 @@
 package color
 
-import (
-  "runtime" // Used to determine operating system.
-)
-
-// Bash coloring
+// Bash/Zsh coloring
 var Reset  = "\033[0m"
 var Red    = "\033[31m"
 var Green  = "\033[32m"
@@ -14,18 +10,3 @@ var Purple = "\033[35m"
 var Cyan   = "\033[36m"
 var Gray   = "\033[37m"
 var White  = "\033[97m"
-
-func init() {
-  if runtime.GOOS == "windows" {
-    // TODO
-      Reset  = ""
-      Red    = ""
-      Green  = ""
-      Yellow = ""
-      Blue   = ""
-      Purple = ""
-      Cyan   = ""
-      Gray   = ""
-      White  = ""
-  }
-}
